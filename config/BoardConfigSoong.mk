6759213f50227882147964f928d3c2949bcc3bc9
johnmart19 <johnivan19999@gmail.com>
2023-05-31 00:01:09 +0300

    vendor: Add TARGET_INCLUDES_MIUI_CAMERA
    - Inspired from 11f751a76380eca5e5bd1b5db329ff9fc4c732df
    
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -56,7 +56,8 @@ SOONG_CONFIG_lineageGlobalVars += \
     target_trust_usb_control_disable \
     uses_egl_display_array \
     uses_oplus_camera \
-    uses_nothing_camera
+    uses_nothing_camera \
+    include_miui_camera
 
 SOONG_CONFIG_NAMESPACES += lineageNvidiaVars
 SOONG_CONFIG_lineageNvidiaVars += \
@@ -91,6 +92,7 @@ SOONG_CONFIG_lineageQcomVars_supports_extended_compress_format := $(AUDIO_FEATUR
 SOONG_CONFIG_lineageQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_lineageGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_lineageGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
+SOONG_CONFIG_lineageGlobalVars_include_miui_camera := $(TARGET_INCLUDES_MIUI_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
