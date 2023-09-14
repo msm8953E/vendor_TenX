99de912f12a33b1be30875846cce2f8262fcdac6
Tim Zimmermann <tim@linux4.de>
2023-09-14 16:38:18 +0530

    vendor: Support charger on vendor
    Change-Id: Id402ddeae8e43392d0a9f505e5cf4c162231d99f
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -140,7 +140,9 @@ PRODUCT_COPY_FILES += \
 
 # Charger
 PRODUCT_PACKAGES += \
-    product_charger_res_images
+    charger_res_images \
+    product_charger_res_images \
+    product_charger_res_images_vendor
 
 # Config
 PRODUCT_PACKAGES += \
