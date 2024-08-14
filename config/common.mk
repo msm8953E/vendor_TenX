1978a23fede48d0987fc309d9de612468ba47e79
Aston-Martinn <advaithbhat9@gmail.com>
2024-08-14 19:53:38 +0530

    vendor: Remove fonts here
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -109,9 +109,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
 
-# Include font files
-include vendor/lineage/config/fonts.mk
-
 # Google Photos Pixel Exclusive XML
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
