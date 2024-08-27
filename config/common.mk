083664da3aedd7bbfa413501449583444c0a2bae
Ghosuto <clash.raja10@gmail.com>
2024-08-27 14:13:06 +0530

    vendor: Optimize more package for speed
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -248,7 +248,11 @@ endif
 
 # SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI
+    SystemUI \
+    Launcher3QuickStep \
+    NexusLauncherRelease \
+    Settings \
+    GameSpace
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.systemuicompilerfilter=speed
