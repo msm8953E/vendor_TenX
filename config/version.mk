32f8d4cc4c17a81bad3d365cddeb4da1efc03ed3
Ghosuto <clash.raja10@gmail.com>
2024-09-02 10:23:45 +0000

    vendor: Fix TenX version
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -2,7 +2,7 @@ PRODUCT_VERSION_MAJOR = 15
 PRODUCT_VERSION_MINOR = 0
 
 # Increase TenX Version with each major release.
-TENX_VERSION := 0.1
+TENX_VERSION := 1.0
 
 # Internal version
 LINEAGE_VERSION := TenX-OS-v$(TENX_VERSION)-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)
